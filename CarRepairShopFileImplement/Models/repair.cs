845405d1702f92ba45b283907789fe7cd4f5a363
Егор Андрианов <goga.andr.gena@mail.ru>
2022-03-12 16:12:49 +0400

    made

---
+++ CarRepairShopFileImplement/Models/repair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepairShopFileImplement.Models
+{
+    public class repair
+    {
+        public int Id { get; set; }
+        public string repairName { get; set; }
+        public decimal Price { get; set; }
+        public Dictionary<int, int> repairComponents { get; set; }
+    }
+}
